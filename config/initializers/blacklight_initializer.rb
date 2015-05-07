00724eed583afb4e9c563c28634d9a0fff00db5b
Wayne Graham <wayne.graham@virginia.edu>
2015-05-07 12:43:43 -0400

    Create new geoblacklight system

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '5cdad7c13f32f4fc280ba1b4141f40c89085083c6daf98637e49b3fb10b89e1a2b1db0dcc126878541d7356bb86937e75190b4b8a28d2621556a41133a5f7358'
+
