a9986445a36d510d20242e105706cd208f2d36a0
Wayne Graham <wayne.graham@virginia.edu>
2015-07-21 12:30:10 -0400

    use my_authentication based on usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,20 @@ class User < ActiveRecord::Base
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
   # the account.
   def to_s
-    email
+    username
+  end
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
   end
 end
