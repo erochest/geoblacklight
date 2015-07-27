0b8b15a21ac14892cc98e6febb59e6825ba208cf
Ammon Shepherd <ammon.shepherd@gmail.com>
2015-07-27 14:59:54 -0400

    Add controllers and routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "catalog#index"
   blacklight_for :catalog
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations', passwords: 'users/passwords' }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
